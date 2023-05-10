5e84a5201239f983b16da03febbfa9fb0b89bf3a
Quoicou <72447511+quoicou@users.noreply.github.com>
2023-05-10 10:11:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import pandas as pd
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from datetime import datetime
+from datetime import datetime, timedelta
+
 
 def start():
-    choix = int(input("Que souhaitez-vous ?\n\n1) Vérifier les attaques de ce jour\n2) Vérifier les attaques d'une date antérieure\n\nChoix : "))
+    choix = int(input(
+        "Que souhaitez-vous ?\n\n1) Vérifier les attaques de ce jour\n2) Vérifier les attaques d'une date antérieure\n3) Vérifier les attaques d'une période\n\nChoix : "))
 
-    while choix not in [1,2]:
+    while choix not in [1, 2, 3]:
         print("\n##################\nERREUR DE SAISIE\n")
-        choix = int(input("Que souhaitez-vous ?\n\n1) Vérifier les attaques de ce jour\n2) Vérifier les attaques d'une date antérieure\n\nChoix : "))
+        choix = int(input(
+            "Que souhaitez-vous ?\n\n1) Vérifier les attaques de ce jour\n2) Vérifier les attaques d'une date antérieure\n3) Vérifier les attaques d'une période\n\nChoix : "))
 
     return choix
+
 
 # Recupérer la date du jour au format aaaa-mm-jj
 def date_aaaa_mm_jj():
     date_du_jour = datetime.today()
 
     return date_du_jour
+
 
 def date_amj_to_jma(date):
     date_convert_type = datetime.strptime(date, "%Y-%m-%d")
@@ -27,10 +32,28 @@ def date_amj_to_jma(date):
 
     return date_format
 
+
 def date_user():
     date = input("\n##################\nQuelle date souhaitez-vous consulter (format aaaa-mm-jj) ?\n\nSaisie : ")
 
     return date
+
+def delta_date():
+    liste_periode = []
+
+    start_date = input("\n##################\nQuelle est la date de début de la période (format aaaa-mm-jj) ?\n\nSaisie : ")
+
+    end_date = input("\n##################\nQuelle est la date de fin de la période (format aaaa-mm-jj) ?\n\nSaisie : ")
+
+    start = datetime.strptime(start_date, '%Y-%m-%d')
+    end = datetime.strptime(end_date, '%Y-%m-%d')
+    delta = timedelta(days=1)
+
+    while start <= end:
+        liste_periode.append(start.strftime('%Y-%m-%d'))
+        start += delta
+
+    return liste_periode
 
 # Scrapping du tableau du site ransomware.live
 def scrapping_raas():
@@ -40,6 +63,7 @@ def scrapping_raas():
     liste_donnees = dataframe[0].values.tolist()
 
     return liste_donnees
+
 
 def nb_raas_annee_actuelle(liste_donnees):
     nb_raas_annee = 0
@@ -51,9 +75,11 @@ def nb_raas_annee_actuelle(liste_donnees):
 
     return nb_raas_annee
 
+
 # Trier la liste en ne gardant que les revendications du jour
 def tri_tableau(liste_donnees, choix):
     liste_raas = []
+    date_fin_periode = 0
 
     if choix == 1:
         date = str(date_aaaa_mm_jj())[:10]
@@ -66,7 +92,7 @@ def tri_tableau(liste_donnees, choix):
 
         nb_raas = len(liste_raas)
 
-    else:
+    elif choix == 2:
         date = date_user()
 
         for donnee in liste_donnees:
@@ -77,7 +103,21 @@ def tri_tableau(liste_donnees, choix):
 
         nb_raas = len(liste_raas)
 
-    return liste_raas, nb_raas, date
+    else:
+        liste_periode = delta_date()
+        date = liste_periode[0]
+        date_fin_periode = liste_periode[-1]
+
+        for date_periode in liste_periode:
+            for donnee in liste_donnees:
+                if date_periode == donnee[2][:10]:
+                    donnee[2] = donnee[2][:10]
+                    liste_raas.append(donnee)
+
+        nb_raas = len(liste_raas)
+
+    return liste_raas, nb_raas, date, date_fin_periode
+
 
 # Affichage de la liste des attaques du jour revendiquées
 def affichage_liste(liste_raas, nb_raas, date, nb_raas_annee):
@@ -88,14 +128,17 @@ def affichage_liste(liste_raas, nb_raas, date, nb_raas_annee):
         print(f"Aucune attaque n'a été revendiquée le {date_format_affichage}")
 
     else:
-        print(f"\n##################\nDepuis début {str(date_aaaa_mm_jj())[:4]}, il y a eu {nb_raas_annee} attaques par ransomware revendiquées\n\nLe {date_format_affichage}, il y a eu {nb_raas} revendications d'attaques par ransomware : \n")
+        print(
+            f"\n##################\nDepuis début {str(date_aaaa_mm_jj())[:4]}, il y a eu {nb_raas_annee} attaques par ransomware revendiquées\n\nLe {date_format_affichage}, il y a eu {nb_raas} revendications d'attaques par ransomware : \n")
 
         while i < nb_raas:
-            print(f"{i+1}) Attaquant : {liste_raas[i][1]} | Organisme attaqué : {liste_raas[i][0]}")
-            i+=1
+            print(f"{i + 1}) Attaquant : {liste_raas[i][1]} | Organisme attaqué : {liste_raas[i][0]}")
+            i += 1
+
 
 def choix_mail():
-    choix_envoi_mail = int(input("\n##################\nSouhaitez-vous envoyer un mail d'alerte ?\n\n1) Oui\n2) Non\n\nChoix : "))
+    choix_envoi_mail = int(
+        input("\n##################\nSouhaitez-vous envoyer un mail d'alerte ?\n\n1) Oui\n2) Non\n\nChoix : "))
 
     while choix_envoi_mail not in [1, 2]:
         print("\n##################\nERREUR DE SAISIE\n")
@@ -103,42 +146,49 @@ def choix_mail():
 
     return choix_envoi_mail
 
+
 # Envoyer un mail de synthèse
-def envoi_mail(nb_raas, liste_raas, date, nb_raas_annee):
+def envoi_mail(nb_raas, liste_raas, date, nb_raas_annee, choix, date_fin_periode):
     i = 0
 
     date_format_mail = date_amj_to_jma(date)
+    date_format_mail_fin_periode = date_amj_to_jma(date_fin_periode)
 
     message = MIMEMultipart()
-    
+
     destinataire = ['INSERER MAIL']
     message["From"] = 'Alerte Ransomware'
-    message["Subject"] = f'Bilan Ransomware - {date_format_mail}'
+    if choix == 3:
+        message["Subject"] = f'Bilan Ransomware - du {date_format_mail} au {date_format_mail_fin_periode}'
+
+    else:
+        message["Subject"] = f'Bilan Ransomware - {date_format_mail}'
 
     if nb_raas == 0:
         msg = f"Depuis début {str(date_aaaa_mm_jj())[:4]}, il y a eu {nb_raas_annee} attaques par ransomware revendiquées\n\nCependant, aujourd'hui aucune attaque n'a été revendiquée le {date_format_mail}."
 
     else:
-        msg = f"<table border: 1px;><thead><tr><th colspan=3>Bilan des {nb_raas} attaques par ransomware du {date_format_mail}</th></tr></thead><tbody>"
+        if choix == 3:
+            msg = f"<table border: 1px;><thead><tr><th colspan=3>Bilan des {nb_raas} attaques par ransomware du {date_format_mail} au {date_format_mail_fin_periode}</th></tr></thead><tbody>"
+        else:
+            msg = f"<table border: 1px;><thead><tr><th colspan=3>Bilan des {nb_raas} attaques par ransomware du {date_format_mail}</th></tr></thead><tbody>"
 
         while i < nb_raas:
-            #msg += f"<br>{i+1}) Attaquant : {liste_raas[i][1]} | Victime : {liste_raas[i][0]}"
-
-            msg += f"<tr><td border: 1px;>{i+1})</td> <td>Attaquant : {liste_raas[i][1]}</td><td>Victime : {liste_raas[i][0]}</td>"
-            i+=1
+            msg += f"<tr><td border: 1px;>{i + 1})</td><td>Attaquant : {liste_raas[i][1]}</td><td>Victime : {liste_raas[i][0]}</td>"
+            i += 1
 
         msg += "</tbody></table>"
 
-        msg += f"<br>PS : Depuis début {str(date_aaaa_mm_jj())[:4]}, il y a eu {nb_raas_annee} attaques par ransomware revendiquées dans le monde<br><br>Cordialement l'outil de monitoring du pôle IT Alcyconie by Juniors<br>"
+        msg += f"<br>PS : Depuis début {str(date_aaaa_mm_jj())[:4]}, il y a eu {nb_raas_annee} attaques par ransomware revendiquées dans le monde<br><br>N'hésitez pas à nous faire vos retours, cela améliorera l'algorithme<br><br>Cordialement l'outil de monitoring du pôle IT Alcyconie by Juniors<br>"
 
     messageText = MIMEText(msg, 'html')
 
     message.attach(messageText)
 
     email = 'INSERER MAIL'
-    password = 'INSERER MDP'
+    password = 'INSERER PSWD'
 
-    server = smtplib.SMTP('smtp.gmail.com:587')
+    server = smtplib.SMTP('smtp.gmail.com:INSRER PORT')
     server.ehlo('Gmail')
     server.starttls()
     server.login(email, password)
@@ -151,17 +201,18 @@ def envoi_mail(nb_raas, liste_raas, date, nb_raas_annee):
 
     server.quit()
 
+
 if __name__ == '__main__':
     choix = start()
     liste_donnees = scrapping_raas()
-    liste_raas, nb_raas, date = tri_tableau(liste_donnees, choix)
+    liste_raas, nb_raas, date, date_fin_periode = tri_tableau(liste_donnees, choix)
     nb_raas_annee = nb_raas_annee_actuelle(liste_donnees)
     affichage_liste(liste_raas, nb_raas, date, nb_raas_annee)
     choix_envoi_mail = choix_mail()
     if choix_envoi_mail == 1:
-        envoi_mail(nb_raas, liste_raas, date, nb_raas_annee)
+        envoi_mail(nb_raas, liste_raas, date, nb_raas_annee, choix, date_fin_periode)
 
-#Ajout dans CaillouNoir
-#choix mail : 1) alcyonie all ou autre mail
-#Ajout schéma des 10 plus gros groupes de l'année genre camembert
-#Ajout nb_raas par groupe sur l'année ou mois
+# Ajout dans CaillouNoir
+# choix mail : 1) alcyonie all ou autre mail
+# Ajout schéma des 10 plus gros groupes de l'année genre camembert
+# Ajout nb_raas par groupe sur l'année ou mois
